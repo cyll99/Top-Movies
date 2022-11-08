2449a0daa77f3d636bfc27ec67f7d9d8d0f2b767
cyll99 <christyanlove@gmail.com>
2022-11-07 18:26:09 -0800

    navigation page

--- Top Movies/DetailPage.xaml.cs
+++ Top Movies/DetailPage.xaml.cs
@@ -3,21 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Timers;
+
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 namespace Top_Movies
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
-    public partial class SplashScreen : ContentPage
+    public partial class DetailPage : ContentPage
     {
-        public SplashScreen()
+        public DetailPage()
         {
             InitializeComponent();
-            Timer timer = new Timer(5000);
-            timer.Start();
-            Navigation.PushAsync(new MainPage());
         }
     }
 }
